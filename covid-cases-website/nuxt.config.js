67fe48eceae36184ab87d58afb03da132d88fb7d
yazui <yazui.zag@gmail.com>
2022-07-23 20:16:13 +0200

    nuxt.config.js updated

--- covid-cases-website/nuxt.config.js
+++ covid-cases-website/nuxt.config.js
@@ -29,23 +29,13 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
