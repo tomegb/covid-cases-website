3a8fedf2dfa79315a43a1604725b47a7d8a7abce
tom.egb <93470251+yazui@users.noreply.github.com>
2022-07-29 23:52:43 +0200

    Delete pages directory

--- pages/dashboard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
-    <title>Dashboard</title>
-	<navbar/>
-    <dashboard_cards/>
-	<main_footer/>
-</template>
