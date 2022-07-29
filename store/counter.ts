798ea7a6c7fb4b378108c052a00a15eadef43ad0
yazui <yazui.zag@gmail.com>
2022-07-29 23:42:38 +0200

    counter.ts added

---
+++ store/counter.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useCounterStore = defineStore('counter', () => {
+	const count = ref(0);
+
+	function decrease() {
+		count.value--;
+	}
+
+	function increase() {
+		count.value++;
+	}
+
+	return { count, decrease, increase };
+});
