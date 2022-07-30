aba3d4ff6acdacbccc8b36dde2552381c0db7976
yazui <yazui.zag@gmail.com>
2022-07-31 01:47:52 +0200

    dashboard_cards.vue updated

--- components/dashboard_cards.vue
+++ components/dashboard_cards.vue
@@ -1,39 +1,199 @@
 <template>
 	<div class="min-h-screen">
+		<div class="... pb-8">
+			<select class="select select-bordered w-full max-w-xs">
+				<option disabled selected>States</option>
+				<option>Baden Württemberg</option>
+				<option>Bayern</option>
+				<option>Berlin</option>
+				<option>Brandenburg</option>
+				<option>Bremen</option>
+				<option>Hamburg</option>
+				<option>Hessen</option>
+				<option>Mecklenburg-Vorpommern</option>
+				<option>Niedersachsen</option>
+				<option>Nordrhein Westfalen</option>
+				<option>Rheinland-Pfalz</option>
+				<option>Saarland</option>
+				<option>Sachsen</option>
+				<option>Sachsen-Anhalt</option>
+				<option>Schleswig-Holstein</option>
+				<option>Thüringen</option>
+			</select>
+		</div>
 		<div class="flex flex-wrap">
-			<div class="flex flex-row">
-				<div class="basis-3/4">
-					<div class="stats shadow">
-						<div class="stat">
-							<div class="stat-title">Total Page Views</div>
-							<div class="stat-value">89,400</div>
-							<div class="stat-desc">21% more than last month</div>
-						</div>
-					</div>
-					<div class="stats shadow">
-						<div class="stat">
-							<div class="stat-title">Total Page Views</div>
-							<div class="stat-value">89,400</div>
-							<div class="stat-desc">21% more than last month</div>
-						</div>
-					</div>
-					<div class="stats shadow">
-						<div class="stat">
-							<div class="stat-title">Total Page Views</div>
-							<div class="stat-value">89,400</div>
-							<div class="stat-desc">21% more than last month</div>
-						</div>
-					</div>
+			<div class="stats shadow">
+				<div class="stat">
+					<div class="stat-title">Total Page Views</div>
+					<div class="stat-value">89,400</div>
+					<div class="stat-desc">21% more than last month</div>
 				</div>
-				<div class="basis-1/4">
-					<div class="stats shadow">
-						<div class="stat">
-							<div class="stat-title">Total Page Views</div>
-							<div class="stat-value">89,400</div>
-							<div class="stat-desc">21% more than last month</div>
-						</div>
-					</div>
+			</div>
+			<div class="stats shadow">
+				<div class="stat">
+					<div class="stat-title">Total Page Views</div>
+					<div class="stat-value">89,400</div>
+					<div class="stat-desc">21% more than last month</div>
 				</div>
+			</div>
+			<div class="stats shadow">
+				<div class="stat">
+					<div class="stat-title">Total Page Views</div>
+					<div class="stat-value">89,400</div>
+					<div class="stat-desc">21% more than last month</div>
+				</div>
+			</div>
+			<div class="stats shadow">
+				<div class="stat">
+					<div class="stat-title">Total Page Views</div>
+					<div class="stat-value">89,400</div>
+					<div class="stat-desc">21% more than last month</div>
+				</div>
+			</div>
+			<div class="radial-progress" style="--value: 70; --size: 8rem; --thickness: 0.7rem">80%</div>
+			<div class="overflow-x-auto">
+				<table class="table w-full">
+					<!-- head -->
+					<thead>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Job</th>
+							<th>Favorite Color</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- row 1 -->
+						<tr>
+							<th>1</th>
+							<td>Cy Ganderton</td>
+							<td>Quality Control Specialist</td>
+							<td>Blue</td>
+						</tr>
+						<!-- row 2 -->
+						<tr>
+							<th>2</th>
+							<td>Hart Hagerty</td>
+							<td>Desktop Support Technician</td>
+							<td>Purple</td>
+						</tr>
+						<!-- row 3 -->
+						<tr>
+							<th>3</th>
+							<td>Brice Swyre</td>
+							<td>Tax Accountant</td>
+							<td>Red</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="overflow-x-auto">
+				<table class="table w-full">
+					<!-- head -->
+					<thead>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Job</th>
+							<th>Favorite Color</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- row 1 -->
+						<tr>
+							<th>1</th>
+							<td>Cy Ganderton</td>
+							<td>Quality Control Specialist</td>
+							<td>Blue</td>
+						</tr>
+						<!-- row 2 -->
+						<tr>
+							<th>2</th>
+							<td>Hart Hagerty</td>
+							<td>Desktop Support Technician</td>
+							<td>Purple</td>
+						</tr>
+						<!-- row 3 -->
+						<tr>
+							<th>3</th>
+							<td>Brice Swyre</td>
+							<td>Tax Accountant</td>
+							<td>Red</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="overflow-x-auto">
+				<table class="table w-full">
+					<!-- head -->
+					<thead>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Job</th>
+							<th>Favorite Color</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- row 1 -->
+						<tr>
+							<th>1</th>
+							<td>Cy Ganderton</td>
+							<td>Quality Control Specialist</td>
+							<td>Blue</td>
+						</tr>
+						<!-- row 2 -->
+						<tr>
+							<th>2</th>
+							<td>Hart Hagerty</td>
+							<td>Desktop Support Technician</td>
+							<td>Purple</td>
+						</tr>
+						<!-- row 3 -->
+						<tr>
+							<th>3</th>
+							<td>Brice Swyre</td>
+							<td>Tax Accountant</td>
+							<td>Red</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="overflow-x-auto">
+				<table class="table w-full">
+					<!-- head -->
+					<thead>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Job</th>
+							<th>Favorite Color</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- row 1 -->
+						<tr>
+							<th>1</th>
+							<td>Cy Ganderton</td>
+							<td>Quality Control Specialist</td>
+							<td>Blue</td>
+						</tr>
+						<!-- row 2 -->
+						<tr>
+							<th>2</th>
+							<td>Hart Hagerty</td>
+							<td>Desktop Support Technician</td>
+							<td>Purple</td>
+						</tr>
+						<!-- row 3 -->
+						<tr>
+							<th>3</th>
+							<td>Brice Swyre</td>
+							<td>Tax Accountant</td>
+							<td>Red</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
