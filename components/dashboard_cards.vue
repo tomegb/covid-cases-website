69f6586e68495a6963608cf3480b9c928e6b0015
yazui <yazui.zag@gmail.com>
2022-07-31 01:04:27 +0200

    dashboard_cards.vue updated

--- components/dashboard_cards.vue
+++ components/dashboard_cards.vue
@@ -1,5 +1,40 @@
 <template>
-	<div class="... m-8">
-		<div class="min-h-screen"></div>
+	<div class="min-h-screen">
+		<div class="flex flex-wrap">
+			<div class="flex flex-row">
+				<div class="basis-3/4">
+					<div class="stats shadow">
+						<div class="stat">
+							<div class="stat-title">Total Page Views</div>
+							<div class="stat-value">89,400</div>
+							<div class="stat-desc">21% more than last month</div>
+						</div>
+					</div>
+					<div class="stats shadow">
+						<div class="stat">
+							<div class="stat-title">Total Page Views</div>
+							<div class="stat-value">89,400</div>
+							<div class="stat-desc">21% more than last month</div>
+						</div>
+					</div>
+					<div class="stats shadow">
+						<div class="stat">
+							<div class="stat-title">Total Page Views</div>
+							<div class="stat-value">89,400</div>
+							<div class="stat-desc">21% more than last month</div>
+						</div>
+					</div>
+				</div>
+				<div class="basis-1/4">
+					<div class="stats shadow">
+						<div class="stat">
+							<div class="stat-title">Total Page Views</div>
+							<div class="stat-value">89,400</div>
+							<div class="stat-desc">21% more than last month</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
