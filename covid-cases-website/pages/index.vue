e5edc01e4f98dae8c0fb50f321bfc8116b7df3a1
yazui <yazui.zag@gmail.com>
2022-08-29 21:38:48 +0200

    initial commit

---
+++ covid-cases-website/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
+	<title>Corona Cases</title>
+	<navbar />
+	<hero />
+	<main_footer />
+</template>
--- pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
-    <title>Corona Cases</title>
-	<navbar/>
-	<hero/>
-	<main_footer/>
-</template>
