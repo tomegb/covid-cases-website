7fe49dc7e73c07d6df77930234131663193ba0c9
Tom Egbers <mail@tomegb.cloud>
2022-10-03 18:57:13 +0200

    Update dashboard.vue

--- covid-cases-website/pages/dashboard.vue
+++ covid-cases-website/pages/dashboard.vue
@@ -1,8 +1,10 @@
 <template>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
-    <title>Dashboard</title>
-	<navbar/>
-    <dashboard_cards/>
-	<main_footer/>
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" type="image/png" href="/static/favicon.png" />
+	<title>Dashboard</title>
+	<navbar />
+	<dashboard_cards />
+	<main_footer />
 </template>
+
+<script></script>
