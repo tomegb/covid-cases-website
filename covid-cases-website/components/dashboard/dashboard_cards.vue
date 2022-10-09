bd703a5a47856103d57779669e6dee3a3913106b
Tom Egbers <mail@tomegb.cloud>
2022-10-09 22:07:40 +0200

    Update dashboard_cards.vue

--- covid-cases-website/components/dashboard/dashboard_cards.vue
+++ covid-cases-website/components/dashboard/dashboard_cards.vue
@@ -1,61 +1,67 @@
 <template>
-		<div class="... pt-12">
-			<div class="... flex justify-center">
-				<div class="flex flex-wrap">
-					<div class="... px-8">
-						<div class="stats bg-base-200 shadow">
-							<div class="stat">
-								<div class="stat-title">Cases</div>
-								<div class="stat-value">
-									<p key="normal.cases" for="data of cases">{{ cases.cases }}</p>
-								</div>
+	<div class="... pt-12">
+		<div class="... flex justify-center">
+			<div class="flex flex-wrap">
+				<div class="... px-8">
+					<div class="stats bg-base-200 shadow">
+						<div class="stat">
+							<div class="stat-title">Cases</div>
+							<div class="stat-value">
+								<p key="normal.cases" for="data of cases">{{ cases.cases }}</p>
+							</div>
+							<!--
 								<div class="stat-desc">
 									<p v-for="delta of cases" :key="delta.cases">{{ delta.cases }}</p>
 								</div>
-							</div>
+								-->
 						</div>
 					</div>
-					<div class="... px-8">
-						<div class="stats bg-base-200 shadow">
-							<div class="stat">
-								<div class="stat-title">Deaths</div>
-								<div class="stat-value">
-									<p key="normal.cases" for="data of cases">{{ cases.deaths }}</p>
-								</div>
+				</div>
+				<div class="... px-8">
+					<div class="stats bg-base-200 shadow">
+						<div class="stat">
+							<div class="stat-title">Deaths</div>
+							<div class="stat-value">
+								<p key="normal.cases" for="data of cases">{{ cases.deaths }}</p>
+							</div>
+							<!--
 								<div class="stat-desc">
 									<p v-for="delta of deaths" :key="delta.deaths">{{ delta.deaths }}</p>
 								</div>
-							</div>
+								-->
 						</div>
 					</div>
-					<div class="... px-8">
-						<div class="stats bg-base-200 shadow">
-							<div class="stat">
-								<div class="stat-title">Recovered</div>
-								<div class="stat-value">
-									<p key="normal.cases" for="data of cases">{{ cases.recovered }}</p>
-								</div>
+				</div>
+				<div class="... px-8">
+					<div class="stats bg-base-200 shadow">
+						<div class="stat">
+							<div class="stat-title">Recovered</div>
+							<div class="stat-value">
+								<p key="normal.cases" for="data of cases">{{ cases.recovered }}</p>
+							</div>
+							<!--
 								<div class="stat-desc">
 									<p v-for="delta of recovered" :key="delta.recovered">{{ delta.recovered }}</p>
 								</div>
-							</div>
+								-->
 						</div>
 					</div>
-					<div class="... px-8">
-						<div class="stats bg-base-200 shadow">
-							<div class="stat">
-								<div class="stat-title">Incidence 7 Days</div>
-								<div class="stat-value">
-									<p v-for="data of incidence7Days" :key="data.incidence7Days">
-										{{ data.incidence7Days }}
-									</p>
-								</div>
+				</div>
+				<div class="... px-8">
+					<div class="stats bg-base-200 shadow">
+						<div class="stat">
+							<div class="stat-title">Incidence 7 Days</div>
+							<div class="stat-value">
+								<p key="data.weekIncidence" for="data of weekIncidence">
+									{{ weekIncidence.weekIncidence }}
+								</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 </template>
 
 <script>
@@ -65,7 +71,11 @@ export default {
 	name: 'DashboardCards',
 	data() {
 		return {
-			cases: []
+			cases: [],
+			deaths: [],
+			recovered: [],
+			weekIncidence: [],
+			lastUpdate: []
 		};
 	},
 	async created() {
@@ -75,12 +85,8 @@ export default {
 			this.cases = res.data;
 			this.deaths = res.data;
 			this.recovered = res.data;
-			this.incidence7Days = res.data;
+			this.weekIncidence = res.data;
 			this.lastUpdate = res.data;
-
-			const res2 = await axios.get(`https://api.corona-zahlen.org/vaccinations`);
-
-			this.total = res2.data;
 		} catch (e) {
 			// eslint-disable-next-line no-console
 			console.error(e);
