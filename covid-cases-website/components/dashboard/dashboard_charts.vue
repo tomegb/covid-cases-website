93edde40df4b40e0cbef6386b565f7c9c69af93b
Tom Egbers <mail@tomegb.cloud>
2022-10-09 22:07:41 +0200

    Create dashboard_charts.vue

---
+++ covid-cases-website/components/dashboard/dashboard_charts.vue
@@ -0,0 +1,32 @@
+<!--
+<template>
+	<div class="... pt-24"></div>
+</template>
+
+<script>
+import axios from 'axios';
+import moment from 'moment';
+
+export default {
+	name: 'DashboardCharts',
+	data() {
+		return {
+			cases: [],
+			date: []
+		};
+	},
+	async created() {
+		try {
+			const { data } = await axios.get(`https://api.corona-zahlen.org/germany/history`);
+
+			data.forEach((d) => {
+				const date = moment(d.date, 'YYYYMMDD').format('MM/DD');
+			});
+		} catch (e) {
+			// eslint-disable-next-line no-console
+			console.error(e);
+		}
+	}
+};
+</script>
+-->
