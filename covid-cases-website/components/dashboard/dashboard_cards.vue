768b06d584e506fd16d9af0f266e2a360e862486
Tom Egbers <mail@tomegb.cloud>
2022-10-09 02:16:22 +0200

    Update dashboard_cards.vue

--- covid-cases-website/components/dashboard/dashboard_cards.vue
+++ covid-cases-website/components/dashboard/dashboard_cards.vue
@@ -1,32 +1,11 @@
 <template>
 	<div class="min-h-screen">
-		<div class="... pb-8">
-			<select class="select select-bordered w-full max-w-xs">
-				<option disabled selected>States</option>
-				<option>Baden Württemberg</option>
-				<option>Bayern</option>
-				<option>Berlin</option>
-				<option>Brandenburg</option>
-				<option>Bremen</option>
-				<option>Hamburg</option>
-				<option>Hessen</option>
-				<option>Mecklenburg-Vorpommern</option>
-				<option>Niedersachsen</option>
-				<option>Nordrhein Westfalen</option>
-				<option>Rheinland-Pfalz</option>
-				<option>Saarland</option>
-				<option>Sachsen</option>
-				<option>Sachsen-Anhalt</option>
-				<option>Schleswig-Holstein</option>
-				<option>Thüringen</option>
-			</select>
-		</div>
 		<div class="flex flex-wrap">
 			<div class="stats shadow">
 				<div class="stat">
 					<div class="stat-title">Cases</div>
 					<div class="stat-value">
-						<p v-for="normal of cases" :key="normal.cases">{{ cases.cases }}</p>
+						<p key="normal.cases" for="data of cases">{{ cases.cases }}</p>
 					</div>
 					<div class="stat-desc">
 						<p v-for="delta of cases" :key="delta.cases">{{ delta.cases }}</p>
@@ -37,7 +16,7 @@
 				<div class="stat">
 					<div class="stat-title">Deaths</div>
 					<div class="stat-value">
-						<p v-for="normal of deaths" :key="normal.deaths">{{ deaths.deaths }}</p>
+						<p key="normal.cases" for="data of cases">{{ cases.deaths }}</p>
 					</div>
 					<div class="stat-desc">
 						<p v-for="delta of deaths" :key="delta.deaths">{{ delta.deaths }}</p>
@@ -48,7 +27,7 @@
 				<div class="stat">
 					<div class="stat-title">Recovered</div>
 					<div class="stat-value">
-						<p v-for="normal of recovered" :key="normal.recovered">{{ recovered.recovered }}</p>
+						<p key="normal.cases" for="data of cases">{{ cases.recovered }}</p>
 					</div>
 					<div class="stat-desc">
 						<p v-for="delta of recovered" :key="delta.recovered">{{ delta.recovered }}</p>
@@ -57,10 +36,10 @@
 			</div>
 			<div class="stats shadow">
 				<div class="stat">
-					<div class="stat-title">Week Incidence</div>
+					<div class="stat-title">Incidence 7 Days</div>
 					<div class="stat-value">
-						<p v-for="data of weekIncidence" :key="data.weekIncidence">
-							{{ weekincidence.weekincidence }}
+						<p v-for="data of incidence7Days" :key="data.incidence7Days">
+							{{ data.incidence7Days }}
 						</p>
 					</div>
 				</div>
@@ -86,12 +65,12 @@ export default {
 			this.cases = res.data;
 			this.deaths = res.data;
 			this.recovered = res.data;
+			this.incidence7Days = res.data;
 			this.lastUpdate = res.data;
-			this.weekincidence = res.data;
 
 			const res2 = await axios.get(`https://api.corona-zahlen.org/vaccinations`);
 
-			this.vaccinated = res2.data;
+			this.total = res2.data;
 		} catch (e) {
 			// eslint-disable-next-line no-console
 			console.error(e);
@@ -99,3 +78,29 @@ export default {
 	}
 };
 </script>
+
+<!--
+
+			<div class="... pb-8">
+			<select class="select select-bordered w-full max-w-xs">
+				<option disabled selected>States</option>
+				<option>Baden Württemberg</option>
+				<option>Bayern</option>
+				<option>Berlin</option>
+				<option>Brandenburg</option>
+				<option>Bremen</option>
+				<option>Hamburg</option>
+				<option>Hessen</option>
+				<option>Mecklenburg-Vorpommern</option>
+				<option>Niedersachsen</option>
+				<option>Nordrhein Westfalen</option>
+				<option>Rheinland-Pfalz</option>
+				<option>Saarland</option>
+				<option>Sachsen</option>
+				<option>Sachsen-Anhalt</option>
+				<option>Schleswig-Holstein</option>
+				<option>Thüringen</option>
+			</select>
+		</div>
+		
+-->
