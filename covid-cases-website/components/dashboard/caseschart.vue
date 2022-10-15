e3d102edd77622be2e2afe71bdad88a979cd44e6
Tom Egbers <mail@tomegb.cloud>
2022-10-15 14:10:27 +0200

    Create caseschart.vue

---
+++ covid-cases-website/components/dashboard/caseschart.vue
@@ -0,0 +1,35 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+	extends: Line,
+	props: {
+		label: {
+			type: String
+		},
+		chartData: {
+			type: ArrayBuffer
+		},
+		options: {
+			type: Object
+		}
+	},
+	mounted() {
+		const dates = this.chartData.map((d) => d.date).reverse();
+		const totals = this.chartData.map((d) => d.total).reverse();
+
+		this.renderChart(
+			{
+				labels: dates,
+				datasets: [
+					{
+						label: this.label,
+						data: totals
+					}
+				]
+			},
+			this.options
+		);
+	}
+};
+</script>
