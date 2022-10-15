135ef44bf4931547b98113d94efd8914945c60bf
Tom Egbers <mail@tomegb.cloud>
2022-10-15 14:10:30 +0200

    Update dashboard_charts.vue

--- covid-cases-website/components/dashboard/dashboard_charts.vue
+++ covid-cases-website/components/dashboard/dashboard_charts.vue
@@ -1,17 +1,27 @@
 <template>
 	<div class="... pt-24"></div>
+	<h2>Cases</h2>
+	<line-chart :chart-data="arrCases" :options="chartOptions" label="Cases"></line-chart>
 </template>
 
 <script>
 import axios from 'axios';
-import moment from 'moment';
+
+import caseschart from './caseschart.vue';
 
 export default {
 	name: 'DashboardCharts',
+	componentes: {
+		caseschart
+	},
 	data() {
 		return {
 			cases: [],
-			date: []
+			date: [],
+			chartOptions: {
+				responsive: true,
+				maintainAspectRatio: false
+			}
 		};
 	},
 	async created() {
@@ -19,7 +29,15 @@ export default {
 			const { data } = await axios.get(`https://api.corona-zahlen.org/germany/history`);
 
 			data.forEach((d) => {
+				// eslint-disable-next-line no-undef
 				const date = moment(d.date, 'YYYYMMDD').format('MM/DD');
+
+				const { cases } = d;
+
+				this.arrCases.push({ date, total: cases });
+
+				// eslint-disable-next-line no-console
+				console.log(this.arrCases);
 			});
 		} catch (e) {
 			// eslint-disable-next-line no-console
